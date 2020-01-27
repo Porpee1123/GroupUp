fb592b53f5e65ff6b43d4ac2c3f303ac88e201b0
IAMP-PC <thanapat.wann@gmail.com>
2020-01-27 23:03:21 +0700

    add hamberger menu not finish

--- app/src/main/java/com/example/groupup/home.java
+++ app/src/main/java/com/example/groupup/home.java
@@ -3,15 +3,22 @@ package com.example.groupup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class home extends AppCompatActivity {
-
+    boolean click = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+    }
+    public void setHumburgerButton(){
+
     }
     public void createGroup(View v){
         Intent intent = new Intent(home.this,createGroup.class);
@@ -21,8 +28,15 @@ public class home extends AppCompatActivity {
 
     }
     public void menuHamberger(View v){
-        Intent intent = new Intent(home.this,register.class);
-        startActivity(intent);
+        FrameLayout fl = (FrameLayout)findViewById(R.id.frameLayout);
+        if (click ==false){
+            fl.setVisibility(View.VISIBLE);
+            fl.bringToFront();
+            click=true;
+        }else {
+            fl.setVisibility(View.INVISIBLE);
+            click=false;
+        }
     }
 
 }
