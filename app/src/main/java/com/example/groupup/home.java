02139ac02afcb62b1171421f8f15460bdf3a4fe9
newNathaporn <nathaporn.buathong@gmail.com>
2020-01-28 12:18:58 +0700

    add menu navigation not select

--- app/src/main/java/com/example/groupup/home.java
+++ app/src/main/java/com/example/groupup/home.java
@@ -2,19 +2,30 @@ package com.example.groupup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-public class home extends AppCompatActivity {
-    boolean click = false;
+import com.google.android.material.navigation.NavigationView;
+
+public class home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = "home";
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.na_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
     public void setHumburgerButton(){
@@ -28,15 +39,30 @@ public class home extends AppCompatActivity {
 
     }
     public void menuHamberger(View v){
-        FrameLayout fl = (FrameLayout)findViewById(R.id.frameLayout);
-        if (click ==false){
-            fl.setVisibility(View.VISIBLE);
-            fl.bringToFront();
-            click=true;
-        }else {
-            fl.setVisibility(View.INVISIBLE);
-            click=false;
-        }
+        drawerLayout.openDrawer(GravityCompat.START);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        item.setChecked(true);
+        switch (item.getItemId()){
+            case R.id.menu_home:
+                Log.d(TAG, "onNavigationItemSelected home: " + item.getTitle());
+            break;
+            case R.id.menu_account:
+                Log.d(TAG, "onNavigationItemSelected account: " + item.getTitle());
+                break;
+            case R.id.menu_calendar:
+                Log.d(TAG, "onNavigationItemSelected calendar: " + item.getTitle());
+                break;
+            case R.id.menu_friend:
+                Log.d(TAG, "onNavigationItemSelected friend: " + item.getTitle());
+                break;
+            case R.id.menu_signout:
+                Log.d(TAG, "onNavigationItemSelected signout: " + item.getTitle());
+                break;
+        }
+        drawerLayout.closeDrawers();
+        return true;
+    }
 }
