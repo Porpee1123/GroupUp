c90cd6c7f75def6e0209f0e296443871442fa916
IAMP-PC <thanapat.wann@gmail.com>
2020-02-26 01:01:56 +0700

    create ManageFriend

---
+++ app/src/main/java/com/example/groupup/ManageFriend.java
@@ -0,0 +1,14 @@
+package com.example.groupup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageFriend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_friend);
+    }
+}
