3ce8149c88347c0cf9abb2e9dec9503a64f4df2c
IAMP-PC <thanapat.wann@gmail.com>
2020-02-25 18:18:44 +0700

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/example/groupup/attandant.java
+++ app/src/main/java/com/example/groupup/attandant.java
@@ -9,11 +9,6 @@ public class attandant extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< Updated upstream:app/src/main/java/com/example/groupup/invitation.java
-        setContentView(R.layout.activity_invitation);
-
-=======
         setContentView(R.layout.activity_attendant);
->>>>>>> Stashed changes:app/src/main/java/com/example/groupup/attandant.java
     }
 }
