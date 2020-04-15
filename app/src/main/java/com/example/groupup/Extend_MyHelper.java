b7ebde84bbfb27f45790e40427dcad5ab41bdf9e
IAMP-PC <thanapat.wann@gmail.com>
2020-04-16 00:19:43 +0700

    add check status priority function

--- app/src/main/java/com/example/groupup/Extend_MyHelper.java
+++ app/src/main/java/com/example/groupup/Extend_MyHelper.java
@@ -3,7 +3,22 @@ package com.example.groupup;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import static android.content.Context.CONNECTIVITY_SERVICE;
 
@@ -21,5 +36,51 @@ public class Extend_MyHelper {
             //ไม่มีการเชื่อมต่อ
         }
         //////////////////////check status internet///////////////////////
+    }
+    public static ArrayList getEventStatusPriorty(String uid,String eid,String pri,Context context){
+        final ArrayList allId = new ArrayList(); //format = eid:statusid:priority->0:1:2
+        final ArrayList<HashMap<String, String>> MyArrList = new ArrayList<HashMap<String, String>>();
+        String url = "http://www.groupupdb.com/android/getidintran.php";
+        url += "?sId=" + uid;//ร  อเอาIdหรือ email จากfirebase
+        url += "&eId=" + eid;
+        url += "&pri=" + pri;
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            HashMap<String, String> map = null;
+                            JSONArray data = new JSONArray(response.toString());
+                            for (int i = 0; i < data.length(); i++) {
+                                JSONObject c = data.getJSONObject(i);
+                                map = new HashMap<String, String>();
+                                map.put("trans_id", c.getString("trans_id"));
+                                map.put("user_id", c.getString("user_id"));
+                                map.put("events_id", c.getString("events_id"));
+                                map.put("states_id", c.getString("states_id"));
+                                map.put("pri_id", c.getString("pri_id"));
+                                map.put("states_name", c.getString("states_name"));
+                                MyArrList.add(map);
+                            }
+                            allId.add(MyArrList.get(0).get("events_id"));
+                            allId.add(MyArrList.get(0).get("states_id"));
+                            allId.add(MyArrList.get(0).get("pri_id"));
+                            allId.add(MyArrList.get(0).get("states_name"));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Log", "Volley::onErrorResponse():" + error.getMessage());
+                    }
+                });
+        RequestQueue queue = Volley.newRequestQueue(context);
+        queue.add(stringRequest);
+        return allId;
     }
 }
