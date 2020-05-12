8389d86dd8a5b5b95071d7768a9fdc95ce04164c
IAMP-PC <thanapat.wann@gmail.com>
2020-05-12 14:38:37 +0700

    add image when create grouup

--- app/src/main/java/com/example/groupup/Home_CreateEvent.java
+++ app/src/main/java/com/example/groupup/Home_CreateEvent.java
@@ -1,19 +1,29 @@
 package com.example.groupup;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
@@ -33,13 +43,28 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class Home_CreateEvent extends AppCompatActivity {
     String name = "", id = "", email = "";
@@ -50,13 +75,18 @@ public class Home_CreateEvent extends AppCompatActivity {
     ArrayList<String> dateLange;
     ProgressDialog progressDialog ;
     private RequestQueue requestQueue;
-
+    final int READ_EXTERNAL_PERMISSION_CODE = 1;
+    ImageButton SelectImageGallery;
+    String ServerUploadPath = "http://www.groupupdb.com/android/addimagecreategroup.php";
+    Bitmap bitmap;
+    boolean check = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Extend_MyHelper.checkInternetLost(this);
         setContentView(R.layout.activity_create_group);
         dateLange = new ArrayList<>();
+        SelectImageGallery = findViewById(R.id.img_slip);
         //Spinner
         Spinner sp = findViewById(R.id.spin_wait);
         edt_bank = findViewById(R.id.edt_bankAccount);
@@ -129,6 +159,22 @@ public class Home_CreateEvent extends AppCompatActivity {
         name = getIntent().getStringExtra("name");
         id = getIntent().getStringExtra("id");
         email = getIntent().getStringExtra("email");
+        SelectImageGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SelectImageGallery.setVisibility(View.VISIBLE);
+                if (ContextCompat.checkSelfPermission(Home_CreateEvent.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(Home_CreateEvent.this, "You have already permission access gallery", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent();
+                    intent.putExtra("email", email + "");
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, "Select Image From Gallery"), 1);
+                } else {
+                    requestImagePermission();
+                }
+            }
+        });
     }
 
     public boolean saveData() {
@@ -244,12 +290,13 @@ public class Home_CreateEvent extends AppCompatActivity {
                                         sped.setSelection(0);
                                         spwa.setSelection(0);
                                         nextNewGroup();
-                                        progressDialog.dismiss();
+//                                        progressDialog.dismiss();
                                     }
 
                                     @Override
                                     protected String doInBackground(Void... params) {
                                         addDateAvaliableTodb();
+                                        ImageUploadToServerFunction(eid);
                                         return "Finish";
                                     }
                                 }
@@ -417,4 +464,218 @@ public class Home_CreateEvent extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(Home_CreateEvent.this);
         queue.add(stringRequest);
     }//add date user create to event
+    @Override
+    protected void onActivityResult(int RC, int RQC, Intent I) {
+
+        super.onActivityResult(RC, RQC, I);
+
+        if (RC == 1 && RQC == RESULT_OK && I != null && I.getData() != null) {
+
+            Uri uri = I.getData();
+
+            try {
+
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+                Bitmap dstBmp;
+                if (bitmap.getWidth() >= bitmap.getHeight()) {
+
+                    dstBmp = Bitmap.createBitmap(
+                            bitmap,
+                            bitmap.getWidth() / 2 - bitmap.getHeight() / 2,
+                            0,
+                            bitmap.getHeight(),
+                            bitmap.getHeight()
+                    );
+
+                } else {
+
+                    dstBmp = Bitmap.createBitmap(
+                            bitmap,
+                            0,
+                            bitmap.getHeight() / 2 - bitmap.getWidth() / 2,
+                            bitmap.getWidth(),
+                            bitmap.getWidth()
+                    );
+                }
+                Bitmap lbp = scaleDown(dstBmp, 375, false);
+
+                SelectImageGallery.setImageBitmap(lbp);
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Bitmap scaleDown(Bitmap realImage, float maxImageSize, boolean filter) {
+        float ratio = Math.min(
+                (float) maxImageSize / realImage.getWidth(),
+                (float) maxImageSize / realImage.getHeight());
+        int width = Math.round((float) ratio * realImage.getWidth());
+        int height = Math.round((float) ratio * realImage.getHeight());
+
+        Bitmap newBitmap = Bitmap.createScaledBitmap(realImage, width,
+                height, filter);
+        return newBitmap;
+    }
+
+    public void requestImagePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            new androidx.appcompat.app.AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed for access the gallery")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(Home_CreateEvent.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_PERMISSION_CODE);
+                        }
+                    }).create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_PERMISSION_CODE);
+        }
+    }
+
+    public void ImageUploadToServerFunction(final String eid) {
+
+        ByteArrayOutputStream byteArrayOutputStreamObject;
+
+        byteArrayOutputStreamObject = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStreamObject);
+        byte[] byteArrayVar = byteArrayOutputStreamObject.toByteArray();
+        final String ConvertImage = Base64.encodeToString(byteArrayVar, Base64.DEFAULT);
+        class AsyncTaskUploadClass extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+
+                super.onPreExecute();
+//                progressDialog = ProgressDialog.show(Home_CreateEvent.this, "Image is Uploading", "Please Wait", false, false);
+            }
+
+            @Override
+            protected void onPostExecute(String string1) {
+
+                super.onPostExecute(string1);
+                finish();
+                // Dismiss the progress dialog after done uploading.
+                progressDialog.dismiss();
+
+                // Printing uploading success message coming from server on android app.
+                Toast.makeText(Home_CreateEvent.this, string1, Toast.LENGTH_LONG).show();
+
+                // Setting image as transparent after done uploading.
+                SelectImageGallery.setImageResource(android.R.color.transparent);
+
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+
+                Home_CreateEvent.ImageProcessClass imageProcessClass = new Home_CreateEvent.ImageProcessClass();
+                HashMap<String, String> HashMapParams = new HashMap<String, String>();
+                HashMapParams.put("eId", eid);
+                HashMapParams.put("photo", ConvertImage);
+                Log.d("hashmap", HashMapParams.toString());
+                String FinalData = imageProcessClass.ImageHttpRequest(ServerUploadPath, HashMapParams);
+                return FinalData;
+            }
+        }
+        AsyncTaskUploadClass AsyncTaskUploadClassOBJ = new AsyncTaskUploadClass();
+        AsyncTaskUploadClassOBJ.execute();
+
+
+    }
+
+    public class ImageProcessClass {
+
+        public String ImageHttpRequest(String requestURL, HashMap<String, String> PData) {
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            try {
+
+                URL url;
+                HttpURLConnection httpURLConnectionObject;
+                OutputStream OutPutStream;
+                BufferedWriter bufferedWriterObject;
+                BufferedReader bufferedReaderObject;
+                int RC;
+
+                url = new URL(requestURL);
+
+                httpURLConnectionObject = (HttpURLConnection) url.openConnection();
+
+                httpURLConnectionObject.setReadTimeout(24000);
+
+                httpURLConnectionObject.setConnectTimeout(24000);
+
+                httpURLConnectionObject.setRequestMethod("POST");
+
+                httpURLConnectionObject.setDoInput(true);
+
+                httpURLConnectionObject.setDoOutput(true);
+
+                OutPutStream = httpURLConnectionObject.getOutputStream();
+
+                bufferedWriterObject = new BufferedWriter(
+
+                        new OutputStreamWriter(OutPutStream, "UTF-8"));
+
+                bufferedWriterObject.write(bufferedWriterDataFN(PData));
+
+                bufferedWriterObject.flush();
+
+                bufferedWriterObject.close();
+
+                OutPutStream.close();
+
+                RC = httpURLConnectionObject.getResponseCode();
+
+                if (RC == HttpsURLConnection.HTTP_OK) {
+
+                    bufferedReaderObject = new BufferedReader(new InputStreamReader(httpURLConnectionObject.getInputStream()));
+
+                    stringBuilder = new StringBuilder();
+
+                    String RC2;
+
+                    while ((RC2 = bufferedReaderObject.readLine()) != null) {
+
+                        stringBuilder.append(RC2);
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return stringBuilder.toString();
+        }
+
+        private String bufferedWriterDataFN(HashMap<String, String> HashMapParams) throws UnsupportedEncodingException {
+
+            StringBuilder stringBuilderObject;
+
+            stringBuilderObject = new StringBuilder();
+
+            for (Map.Entry<String, String> KEY : HashMapParams.entrySet()) {
+
+                if (check)
+
+                    check = false;
+                else
+                    stringBuilderObject.append("&");
+
+                stringBuilderObject.append(URLEncoder.encode(KEY.getKey(), "UTF-8"));
+
+                stringBuilderObject.append("=");
+
+                stringBuilderObject.append(URLEncoder.encode(KEY.getValue(), "UTF-8"));
+            }
+
+            return stringBuilderObject.toString();
+        }
+
+    }
 }
