c54e854055058f812563b24bcc78bcbf11b90b4a
IAMP-PC <thanapat.wann@gmail.com>
2020-05-13 14:39:42 +0700

    add resource for attend

--- app/src/main/java/com/example/groupup/MainAttendent_Reviews.java
+++ app/src/main/java/com/example/groupup/MainAttendent_Reviews.java
@@ -4,15 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainAttendent_Reviews extends AppCompatActivity {
     String id = "", eId = "", eName = "", email;
+    TextView tvName;
+    RatingBar rtBar;
+    EditText edt_datail ;
+    Button btn_con;
+    String textReview="",nameReview="";
+    float scoreReview=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Extend_MyHelper.checkInternetLost(this);
         setContentView(R.layout.activity_attend_reviews);
+        tvName =findViewById(R.id.review_name);
+        rtBar = findViewById(R.id.review_ratingBar);
+        edt_datail = findViewById(R.id.review_edtdetail);
+        btn_con =findViewById(R.id.review_btnConfirm);
         id = getIntent().getStringExtra("id");
         eName = getIntent().getStringExtra("nameEvent");
         eId = getIntent().getStringExtra("eid");
